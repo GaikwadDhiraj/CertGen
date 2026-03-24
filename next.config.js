bd9cfad26402a6a8b098d23c8738329bae704146
GaikwadDhiraj <dvgaikwad333@gmail.com>
2026-03-24 15:01:19 +0530

    deployee1

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+    },
+  ],
+},
 };
 
 export default nextConfig;
